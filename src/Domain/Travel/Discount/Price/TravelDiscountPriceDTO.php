8ee46f8d0fffd75bccaf0ac4f6b733868fac37e6
Sheetov Alexey <50871152+lehenskiy@users.noreply.github.com>
2024-02-16 19:13:34 +0700

    Add API request DTO

---
+++ src/Domain/Travel/Discount/Price/TravelDiscountPriceDTO.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Travel\Discount\Price;
+
+use DateTimeImmutable;
+use Symfony\Component\Validator\Constraints as Assert;
+
+readonly class TravelDiscountPriceDTO
+{
+    #[Assert\GreaterThanOrEqual(1000, message: 'Price should be more than 1000')]
+    public int $travelPrice;
+
+    #[Assert\LessThanOrEqual('today', message: 'Birth date value should be less than today')]
+    public DateTimeImmutable|false $clientBirthDate;
+
+    #[Assert\Range(
+        notInRangeMessage: 'Start date value should be from current year or not later than next year',
+        invalidDateTimeMessage: 'Invalid date(format: dd-mm-YYYY)',
+        min: 'first day of January',
+        max: 'last day of December next year',
+    )]
+    public DateTimeImmutable|false $travelStartDate;
+
+    #[Assert\AtLeastOneOf(constraints: [
+        new Assert\Range(
+            notInRangeMessage: 'Payment date value should be from current year or not later than next year',
+            invalidDateTimeMessage: 'Invalid date(format: dd-mm-YYYY)',
+            min: 'first day of January',
+            max: 'last day of December next year',
+        ),
+        new Assert\IsNull(),
+    ])]
+    #[Assert\AtLeastOneOf(constraints: [
+        new Assert\LessThanOrEqual(
+            propertyPath: 'travelStartDate',
+            message: 'Payment date should be less than or equal travel start date',
+        ),
+        new Assert\IsNull(),
+    ])]
+    public DateTimeImmutable|false|null $travelPaymentDate;
+
+    public function __construct(
+        int $price,
+        string $birthDate,
+        ?string $startDate,
+        ?string $paymentDate,
+    ) {
+        $this->travelPrice = $price;
+        $this->clientBirthDate = DateTimeImmutable::createFromFormat('Y-m-d', $birthDate);
+        $this->travelStartDate = ($startDate === null)
+            ? new DateTimeImmutable()
+            : DateTimeImmutable::createFromFormat('Y-m-d', $startDate);
+        $this->travelPaymentDate = ($paymentDate === null)
+            ? null
+            : DateTimeImmutable::createFromFormat('Y-m-d', $paymentDate);
+    }
+}
