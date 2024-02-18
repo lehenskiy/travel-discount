57ad83ed5ff8b41227ea05a0822247430f1eafc8
Sheetov Alexey <50871152+lehenskiy@users.noreply.github.com>
2024-02-19 03:06:36 +0700

    Update TravelDiscountPriceCalculator.php

--- src/Domain/Travel/Discount/Price/TravelDiscountPriceCalculator.php
+++ src/Domain/Travel/Discount/Price/TravelDiscountPriceCalculator.php
@@ -5,31 +5,29 @@ declare(strict_types=1);
 namespace App\Domain\Travel\Discount\Price;
 
 use App\Api\Travel\Discount\Price\TravelDiscountPriceDTO;
-use App\Domain\Travel\Discount\TravelChildDiscount;
-use App\Domain\Travel\Discount\TravelEarlyBookingDiscountCalculator;
+use App\Domain\Travel\Discount\ChildDiscountCalculator;
+use App\Domain\Travel\Discount\EarlyBookingDiscountCalculator;
 
 class TravelDiscountPriceCalculator
 {
     public function __construct(
-        private TravelChildDiscount $childDiscount,
-        private TravelEarlyBookingDiscountCalculator $earlyBookingDiscount
+        private ChildDiscountCalculator $childDiscount,
+        private EarlyBookingDiscountCalculator $earlyBookingDiscount
     ) {
     }
 
     public function calculateTravelWithDiscountPrice(TravelDiscountPriceDTO $travelDiscountPriceDTO): int
     {
-        $travelPrice = $travelDiscountPriceDTO->travelPrice;
-        $travelPrice -= $this->childDiscount->calculate(
-            $travelDiscountPriceDTO->clientBirthDate,
-            $travelDiscountPriceDTO->travelPaymentDate,
-            $travelPrice
+        $travelPriceToCalculate = $travelDiscountPriceDTO->travelPrice;
+        $travelPriceToCalculate -= $this->childDiscount->calculate($travelDiscountPriceDTO);
+        $travelDiscountPriceDTOWithUpdatedPrice = new TravelDiscountPriceDTO(
+            $travelPriceToCalculate,
+            $travelDiscountPriceDTO->clientBirthDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $travelDiscountPriceDTO->travelStartDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $travelDiscountPriceDTO->travelPaymentDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
-        $travelPrice -= $this->earlyBookingDiscount->calculate(
-            $travelDiscountPriceDTO->travelStartDate,
-            $travelDiscountPriceDTO->travelPaymentDate,
-            $travelPrice
-        );
+        $travelPriceToCalculate -= $this->earlyBookingDiscount->calculate($travelDiscountPriceDTOWithUpdatedPrice);
 
-        return $travelPrice;
+        return $travelPriceToCalculate;
     }
 }
