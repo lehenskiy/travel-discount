f37b56f27cfab3987d322e921b7d9238701a8281
Sheetov Alexey <50871152+lehenskiy@users.noreply.github.com>
2024-02-19 03:06:45 +0700

    Tests update

--- tests/Integration/Travel/Discount/TravelEarlyBookingDiscountTest.php
+++ tests/Integration/Travel/Discount/TravelEarlyBookingDiscountTest.php
@@ -4,59 +4,66 @@ declare(strict_types=1);
 
 namespace Tests\Integration\Travel\Discount;
 
-use App\Domain\Travel\Discount\TravelEarlyBookingDiscountCalculator;
+use App\Api\Travel\Discount\Price\TravelDiscountPriceDTO;
+use App\Domain\Travel\Discount\EarlyBookingDiscountCalculator;
 use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TravelEarlyBookingDiscountTest extends KernelTestCase
 {
+    private const MAX_EARLY_BOOKING_DISCOUNT = 1500;
+
     private int $currentYear;
     private int $nextYear;
-    private TravelEarlyBookingDiscountCalculator $travelEarlyBookingDiscountCalculator;
-
-    private const MAX_EARLY_BOOKING_DISCOUNT = 1500;
+    private EarlyBookingDiscountCalculator $travelEarlyBookingDiscountCalculator;
 
     public function testTravelWithLatePaymentReturnsZeroDiscount(): void
     {
-        $aprilNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-04-01');
         $initialTravelPrice = 1000;
-
+        $aprilNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-04-01');
         $februaryNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-02-01');
-        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate(
-            $aprilNextYearDate,
-            $februaryNextYearDate,
-            $initialTravelPrice
+        $inputDTO = new TravelDiscountPriceDTO(
+            $initialTravelPrice,
+            $aprilNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $aprilNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $februaryNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
+
+        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate($inputDTO);
 
         self::assertSame(0, $actualDiscount);
     }
 
     public function testTravelDiscountNotMoreThanMaxDiscount(): void
     {
-        $aprilNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-04-01');
         $veryBigTravelPrice = 10000000;
-
+        $aprilNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-04-01');
         $novemberThisYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->currentYear . '-11-30');
-        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate(
-            $aprilNextYearDate,
-            $novemberThisYearDate,
-            $veryBigTravelPrice
+        $inputDTO = new TravelDiscountPriceDTO(
+            $veryBigTravelPrice,
+            $aprilNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $aprilNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $novemberThisYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
+
+        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate($inputDTO);
 
         self::assertSame(self::MAX_EARLY_BOOKING_DISCOUNT, $actualDiscount);
     }
 
     public function testTravelStartingInAprilNextYearSevenPercentDiscount(): void
     {
-        $aprilNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-04-01');
         $initialTravelPrice = 1000;
-
+        $aprilNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-04-01');
         $novemberThisYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->currentYear . '-11-30');
-        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate(
-            $aprilNextYearDate,
-            $novemberThisYearDate,
-            $initialTravelPrice
+        $inputDTO = new TravelDiscountPriceDTO(
+            $initialTravelPrice,
+            $aprilNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $aprilNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $novemberThisYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
+
+        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate($inputDTO);
 
         $expectedSevenPercentDiscount = (int)($initialTravelPrice * 0.07);
         self::assertSame($expectedSevenPercentDiscount, $actualDiscount);
@@ -64,15 +71,17 @@ class TravelEarlyBookingDiscountTest extends KernelTestCase
 
     public function testTravelStartingInOctoberThisYearFivePercentDiscount(): void
     {
-        $octoberThisYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->currentYear . '-10-01');
         $initialTravelPrice = 1000;
-
+        $octoberThisYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->currentYear . '-10-01');
         $aprilThisYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->currentYear . '-04-30');
-        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate(
-            $octoberThisYearDate,
-            $aprilThisYearDate,
-            $initialTravelPrice
+        $inputDTO = new TravelDiscountPriceDTO(
+            $initialTravelPrice,
+            $octoberThisYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $octoberThisYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $aprilThisYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
+
+        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate($inputDTO);
 
         $expectedFivePercentDiscount = (int)($initialTravelPrice * 0.05);
         self::assertSame($expectedFivePercentDiscount, $actualDiscount);
@@ -80,15 +89,17 @@ class TravelEarlyBookingDiscountTest extends KernelTestCase
 
     public function testTravelStartingFifteenthJanuaryNextYearThreePercentDiscount(): void
     {
-        $fifteenthJanuaryNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-01-15');
         $initialTravelPrice = 1000;
-
+        $fifteenthJanuaryNextYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->nextYear . '-01-15');
         $octoberThisYearDate = DateTimeImmutable::createFromFormat('Y-m-d', $this->currentYear . '-10-31');
-        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate(
-            $fifteenthJanuaryNextYearDate,
-            $octoberThisYearDate,
-            $initialTravelPrice
+        $inputDTO = new TravelDiscountPriceDTO(
+            $initialTravelPrice,
+            $fifteenthJanuaryNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $fifteenthJanuaryNextYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $octoberThisYearDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
+
+        $actualDiscount = $this->travelEarlyBookingDiscountCalculator->calculate($inputDTO);
 
         $expectedThreePercentDiscount = (int)($initialTravelPrice * 0.03);
         self::assertSame($expectedThreePercentDiscount, $actualDiscount);
@@ -102,6 +113,6 @@ class TravelEarlyBookingDiscountTest extends KernelTestCase
         $this->currentYear = (int)(new DateTimeImmutable())->format('Y');
         $this->nextYear = $this->currentYear + 1;
 
-        $this->travelEarlyBookingDiscountCalculator = $container->get(TravelEarlyBookingDiscountCalculator::class);
+        $this->travelEarlyBookingDiscountCalculator = $container->get(EarlyBookingDiscountCalculator::class);
     }
 }
