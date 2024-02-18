cd3d07525652c0195d8daaa2ea4840698cae063c
Sheetov Alexey <50871152+lehenskiy@users.noreply.github.com>
2024-02-19 03:06:45 +0700

    Tests update

--- tests/Unit/Travel/Discount/TravelChildDiscountTest.php
+++ tests/Unit/Travel/Discount/TravelChildDiscountTest.php
@@ -4,27 +4,31 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Travel\Discount;
 
-use App\Domain\Travel\Discount\TravelChildDiscount;
+use App\Api\Travel\Discount\Price\TravelDiscountPriceDTO;
+use App\Domain\Travel\Discount\ChildDiscountCalculator;
 use DateInterval;
 use PHPUnit\Framework\TestCase;
 
 class TravelChildDiscountTest extends TestCase
 {
-    private TravelChildDiscount $travelChildDiscount;
     private const ADULT_AGE = 18;
     private const MAX_6_YEARS_OLD_CLIENT_DISCOUNT = 4500;
+
+    private ChildDiscountCalculator $travelChildDiscount;
 
     public function testCalculateWithClientAgeLessThanThreeReturnsZero(): void
     {
         $paymentDate = new \DateTimeImmutable();
         // see https://en.wikipedia.org/wiki/ISO_8601#Durations
         $clientBirthDateWithLessThanThreeYearsOldAge = $paymentDate->sub(new DateInterval('P2Y'));
-
-        $result = $this->travelChildDiscount->calculate(
-            $clientBirthDateWithLessThanThreeYearsOldAge,
-            $paymentDate,
-            1000
+        $inputDTO = new TravelDiscountPriceDTO(
+            1000,
+            $clientBirthDateWithLessThanThreeYearsOldAge->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $paymentDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $paymentDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
+
+        $result = $this->travelChildDiscount->calculate($inputDTO);
 
         $this->assertSame(0, $result);
     }
@@ -32,13 +36,15 @@ class TravelChildDiscountTest extends TestCase
     public function testCalculateWithAdultClientReturnsZero(): void
     {
         $paymentDate = new \DateTimeImmutable();
-        $adultClient = $paymentDate->sub(new DateInterval('P' . self::ADULT_AGE . 'Y'));
-
-        $result = $this->travelChildDiscount->calculate(
-            $adultClient,
-            $paymentDate,
-            1000
+        $adultClientBirthDate = $paymentDate->sub(new DateInterval('P' . self::ADULT_AGE . 'Y'));
+        $inputDTO = new TravelDiscountPriceDTO(
+            1000,
+            $adultClientBirthDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $paymentDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $paymentDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
+
+        $result = $this->travelChildDiscount->calculate($inputDTO);
 
         $this->assertSame(0, $result);
     }
@@ -47,19 +53,21 @@ class TravelChildDiscountTest extends TestCase
     {
         $paymentDate = new \DateTimeImmutable();
         $sixYearsOldClient = $paymentDate->sub(new DateInterval('P6Y'));
-
         $veryBigTravelPrice = 100000000;
-        $result = $this->travelChildDiscount->calculate(
-            $sixYearsOldClient,
-            $paymentDate,
-            $veryBigTravelPrice
+        $inputDTO = new TravelDiscountPriceDTO(
+            $veryBigTravelPrice,
+            $sixYearsOldClient->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $paymentDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
+            $paymentDate->format(TravelDiscountPriceDTO::DATE_TIME_INPUT_FORMAT),
         );
+
+        $result = $this->travelChildDiscount->calculate($inputDTO);
 
         $this->assertSame(self::MAX_6_YEARS_OLD_CLIENT_DISCOUNT, $result);
     }
 
     protected function setUp(): void
     {
-        $this->travelChildDiscount = new TravelChildDiscount();
+        $this->travelChildDiscount = new ChildDiscountCalculator();
     }
 }
